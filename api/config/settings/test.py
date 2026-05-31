050613c38f1c1ee14d553d1ac0e4c91754a21239
Erigi Ifiemi <erigiifiemi@gmail.com>
2026-05-31 23:55:49 +0100

    set the scaffold for a test of app "core"; created fixture json files, test python packages, test settings

---
+++ api/config/settings/test.py
@@ -0,0 +1,19 @@
+from .base import BASE_DIR 
+from .base import *
+
+
+# Use sqlite for tests
+DATABASES = {
+    'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': BASE_DIR / 'test_db.sqlite3',
+     }
+}
+
+# Fixture Directories and Files
+FIXTURE_DIRS = [
+    BASE_DIR/ 'fixtures',
+]
+
+# Overwrite DEBUG
+DEBUG = True
